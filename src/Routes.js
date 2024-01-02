fb5957256a40be11054bfb9c43732499b364370f
Piotr Majkutewicz <piotr.majkutewicz@stxnext.pl>
2024-01-02 15:25:47 +0100

    Not Found component has been implemented (also in Router)

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import Earthquakes from "components/Earthquakes/Earthquakes";
 import LicencePlates from "components/LicencePlates/LicencePlates";
+import NotFound from "components/NotFound/NotFound";
 import Welcome from "components/Welcome/Welcome";
 import { Routes, Route } from "react-router-dom";
 
@@ -17,6 +18,10 @@ const getRoutes = () => {
       path: "/earthquakes",
       Component: Earthquakes,
     },
+    {
+      path: '*',
+      Component: NotFound
+    }
   ];
 };
 
